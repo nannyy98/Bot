384447013ba00b94df1b60c91371e7e83d4e1607
nannyy98 <83236945+nannyy98@users.noreply.github.com>
2025-10-05 19:16:55 +0500

    Update main.py

--- Bot-main/main.py
+++ Bot-main/main.py
@@ -11,6 +11,7 @@ import time
 import signal
 import sys
 import threading
+import health_check as _hc  # <--- добавить
 from database import DatabaseManager
 from handlers import MessageHandler
 from notifications import NotificationManager
